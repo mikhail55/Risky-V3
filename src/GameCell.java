9e226053bbb2b070c2813c01f779ca05987efa13
Mikhail Pyatakhin <mikhail.pyatakhin@ashbury.ca>
2019-02-14 11:27:03 -0500

    Populated GameLogic class with variables and empty methods Created RiskyFileReader class

--- src/GameCell.java
+++ src/GameCell.java
@@ -1,6 +1,7 @@
 public class GameCell {
 
     private int numTroops;
+    private int addedTroops;
 
     enum Owner{
      Team1, Team2, Team3, Team4
@@ -9,6 +10,31 @@ public class GameCell {
     private Owner ownerEndTurn;
 
     private boolean isWater;
-    private int addedTroops;
 
+    public GameCell() {
+    }
+
+    public int getNumTroops() {
+        return numTroops;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public boolean isWater() {
+        return isWater;
+    }
+
+    public void setNumTroops(int numTroops) {
+        this.numTroops = numTroops;
+    }
+
+    public void setAddedTroops(int addedTroops) {
+        this.addedTroops = addedTroops;
+    }
+
+    public void setOwnerEndTurn(Owner ownerEndTurn) {
+        this.ownerEndTurn = ownerEndTurn;
+    }
 }
--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,2 +1,40 @@
 public class GameLogic {
+
+    private GameCell[][] board;
+
+    private GameCell.Owner currentPlayer;
+
+    private GameCell.Owner[] players;
+
+    private RiskyFileReader fileReader;
+
+    public GameLogic() {
+    }
+
+    public void move(GameCell from, GameCell to, int numTroops){
+    }
+
+    public void attack(GameCell attacker, GameCell defender, int numTroops){
+
+    }
+
+    public  void deploy(GameCell selectedCell){
+
+    }
+
+    private boolean checkAdjacent(GameCell origin, GameCell destination){
+        return true;
+    }
+
+    public int getEarnedTroops(){
+        return 0;
+    }
+
+    public void endTurn(){
+
+    }
+
+    public GameCell[][] getBoard() {
+        return board;
+    }
 }
