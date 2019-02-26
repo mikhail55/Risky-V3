55200128a9253fa4aaf5564884c059b533431639
rafael <rafael.riosgarcia@ashbury.ca>
2019-02-26 13:49:26 -0500

    added  comments, and new constructor to cell class

--- src/GameCell.java
+++ src/GameCell.java
@@ -9,8 +9,11 @@ public class GameCell {
     private int numTroops;
     private int addedTroops;
 
+    /**
+     * enum is used to dictate what team the cell belongs to
+     */
     enum Owner{
-     Team1, Team2, Team3, Team4
+     Team1, Team2, Team3, Team4, Water,
     }
     private Owner owner;
     private Owner ownerEndTurn;
@@ -18,8 +21,14 @@ public class GameCell {
 
     private boolean isWater;
 
-    public GameCell() {
-        ownerEndTurn = owner;
+    /**
+     * This constructor is dedicated to requesting the values required to initialize a cell
+     * @param cellOwner this parameter asks whether the cell belongs to any team, or if it is water
+     * @param numTroops this parameter initializes the number of troops that each cell will have
+     */
+    public GameCell(Owner cellOwner, int numTroops) {
+        this.owner = cellOwner;
+        this.numTroops = numTroops;
     }
 
     public void update(){
@@ -49,8 +58,9 @@ public class GameCell {
         this.addedTroops = addedTroops;
     }
 
-    public void setOwnerEndTurn(Owner ownerEndTurn) {
-        this.ownerEndTurn = ownerEndTurn;
+    public void setOwnerEndTurn(Owner OwnerEndTurn) {
+        this.ownerEndTurn = OwnerEndTurn;
+        ownerEndTurn = owner;
     }
 
     public Point getCoordinates() {
