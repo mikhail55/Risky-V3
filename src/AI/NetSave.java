8fb7ba12491f0ce5d047ad9834b583ef4d9904b0
TylerWilson <Tyler.Wilson@ashbury.ca>
2019-03-04 11:11:22 -0500

    Made save function for neural networks. Still have no idea if it works

--- src/AI/NetSave.java
+++ src/AI/NetSave.java
@@ -16,8 +16,8 @@ class NetSave {
     NetSave() { }
 
     // I'M SO SORRY I HATE THE SCARY ARRAY TOO
-    int[][][] readNetFile(String fileName) {
-        int[][][] weights = new int[0][0][0];
+    int[][][][] readNetFile(String fileName) {
+        int[][][][] weights = new int[0][0][0][0];
 
         try {
 
@@ -26,23 +26,26 @@ class NetSave {
 
             fileInput.useDelimiter("ø");
 
-            weights = new int[Integer.parseInt(fileInput.next())][][];
+            weights = new int[Integer.parseInt(fileInput.next())][][][];
             fileInput.next();
 
-
-            // First layer is which layer of weights
-            // Second layer is which node
-            // Third layer is each weight
-            for(int i = 0; i < weights.length; i++) {
-                weights[i] = new int[Integer.parseInt(fileInput.next())][];
-                for(int n = 0; n < weights[i].length; n++) {
-                    weights[i][n] = new int[Integer.parseInt(fileInput.next())];
-                    for(int j = 0; j < weights[j][n].length; j++) {
-                        weights[i][n][j] = Integer.parseInt(fileInput.next());
+            // First layer is which network
+            // Second layer is which layer of weights
+            // Third layer is which node
+            // Fourth layer is each weight
+            for(int k = 0; k < weights.length; k++) {
+                weights[k] = new int[Integer.parseInt(fileInput.next())][][];
+                for (int i = 0; i < weights[k].length; i++) {
+                    weights[k][i] = new int[Integer.parseInt(fileInput.next())][];
+                    for (int n = 0; n < weights[k][i].length; n++) {
+                        weights[k][i][n] = new int[Integer.parseInt(fileInput.next())];
+                        for (int j = 0; j < weights[k][i][n].length; j++) {
+                            weights[k][i][n][j] = Integer.parseInt(fileInput.next());
+                        }
                     }
-                }
 
-                fileInput.next();
+                    fileInput.next();
+                }
             }
 
             fileInput.close();
@@ -58,5 +61,40 @@ class NetSave {
         }
 
         return weights;
+    }
+
+    void saveFile(String fileName, int[][][][] weights) {
+        try {
+            FileWriter fw = new FileWriter(fileName, false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter outFile = new PrintWriter(bw);
+
+            outFile.println(weights.length + "ø");
+
+            outFile.println();
+
+            for(int k = 0; k < weights.length; k++) {
+                outFile.print(weights[k].length + "ø");
+                for (int i = 0; i < weights[k].length; i++) {
+                    outFile.print(weights[k][i].length + "ø");
+                    for (int n = 0; n < weights[k][i].length; n++) {
+                        weights[k][i][n] = new int[Integer.parseInt(fileInput.next())];
+                        outFile.print(weights[k][i][n].length + "ø");
+                        for (int j = 0; j < weights[k][i][n].length; j++) {
+                            outFile.print(weights[k][i][n][j] + "ø");
+                        }
+                    }
+
+                    outFile.println();
+                }
+            }
+
+            outFile.close();
+
+        } catch (Exception e) {
+            System.err.println("Error");
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 }
