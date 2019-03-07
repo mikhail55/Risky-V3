a47a3c5a05b65b909b220f8fcf891ab40557b122
TylerWilson <Tyler.Wilson@ashbury.ca>
2019-03-06 22:33:30 -0500

    Finished up neural save function

--- src/AI/NetSave.java
+++ src/AI/NetSave.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.nio.ByteBuffer;
+import java.nio.*;
 import java.util.Scanner;
 
 /**
@@ -42,11 +42,41 @@ public class NetSave {
                         }
                     }
                 }
-
-                if(fileInput.hasNext()) {
-                    fileInput.nextLine();
-                }
             }
+
+
+            /*byte[] bytes = new byte[4];
+
+            for(int b = 0; b < 4; b++) {
+                bytes[b] = fileInput.nextByte();
+            }
+            weights = new float[(int) byteArrayToFloat(bytes)][][][];
+
+            for(int k = 0; k < weights.length; k++) {
+                for(int b = 0; b < 4; b++) {
+                    bytes[b] = fileInput.nextByte();
+                }
+                weights[k] = new float[(int) byteArrayToFloat(bytes)][][];
+                for (int i = 0; i < weights[k].length; i++) {
+                    for(int b = 0; b < 4; b++) {
+                        bytes[b] = fileInput.nextByte();
+                    }
+                    weights[k][i] = new float[(int) byteArrayToFloat(bytes)][];
+                    for (int n = 0; n < weights[k][i].length; n++) {
+                        for(int b = 0; b < 4; b++) {
+                            bytes[b] = fileInput.nextByte();
+                        }
+                        weights[k][i][n] = new float[(int) byteArrayToFloat(bytes)];
+                        for (int j = 0; j < weights[k][i][n].length; j++) {
+                            for(int b = 0; b < 4; b++) {
+                                bytes[b] = fileInput.nextByte();
+                            }
+                            weights[k][i][n][j] = byteArrayToFloat(bytes);
+                        }
+                    }
+                }
+            }*/
+
 
             fileInput.close();
 
@@ -83,12 +113,54 @@ public class NetSave {
                 }
             }
 
+
+            /*byte[] bytes = floatToByteArray(weights.length);
+            for(int b = 0; b < 4; b++) {
+                outFile.print(bytes[b] + " ");
+            }
+            for(int k = 0; k < weights.length; k++) {
+                bytes = floatToByteArray(weights[k].length);
+                for(int b = 0; b < 4; b++) {
+                    outFile.print(bytes[b] + " ");
+                }
+                for (int i = 0; i < weights[k].length; i++) {
+                    bytes = floatToByteArray(weights[k][i].length);
+                    for(int b = 0; b < 4; b++) {
+                        outFile.print(bytes[b] + " ");
+                    }
+                    for (int n = 0; n < weights[k][i].length; n++) {
+                        bytes = floatToByteArray(weights[k][i][n].length);
+                        for(int b = 0; b < 4; b++) {
+                            outFile.print(bytes[b] + " ");
+                        }
+                        for (int j = 0; j < weights[k][i][n].length; j++) {
+                            bytes = floatToByteArray(weights[k][i][n][j]);
+                            for(int b = 0; b < 4; b++) {
+                                outFile.print(bytes[b] + " ");
+                            }
+                        }
+                    }
+                }
+            }*/
+
+
             outFile.close();
+
+
 
         } catch (Exception e) {
             System.err.println("Error writing weights to file");
             e.printStackTrace();
             System.exit(-1);
         }
+    }
+
+    private byte[] floatToByteArray(float f) {
+        return ByteBuffer.allocate(4).putFloat(f).array();
+    }
+
+    private float byteArrayToFloat(byte[] b) {
+        ByteBuffer buff = ByteBuffer.wrap(b);
+        return buff.getFloat();
     }
 }
